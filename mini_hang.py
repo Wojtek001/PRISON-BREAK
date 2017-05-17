5f368e24ab097f207c5eace14d255e719d5b0dac
Wojtek001 <wojtekszczecina@gmail.com>
2017-05-17 10:59:12 +0200

    another mini game

---
+++ mini_hang.py
@@ -0,0 +1,112 @@
+import time
+import sys
+import random
+import os
+
+# list of hangman elements ;-)
+arts = ["-----\n|   |\n|\n|\n|\n|\n|\n|\n|\n--------",
+        "-----\n|   |\n|   0\n|\n|\n|\n|\n|\n|\n--------",
+        "-----\n|   |\n|   0\n|  -+-\n|\n|\n|\n|\n|\n--------",
+        "-----\n|   |\n|   0\n| /-+-\n|\n|\n|\n|\n|\n--------",
+        "-----\n|   |\n|   0\n| /-+-\ \n|   |\n|\n|\n|\n|\n--------",
+        "-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|\n|\n|\n--------",
+        "-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|  |\n|\n|\n--------",
+        "-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|  |\n|  |\n|\n--------",
+        "-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|  | |\n|  |\n|\n--------",
+        "-----\n|   |\n|   0\n| /-+-\ \n|   |\n|   |\n|  | |\n|  | |\n|\n--------"]
+
+
+def get_capital():
+
+    """choose random capital from list"""
+    capitals = ('VIENNA', 'BRUSSELS', 'SOFIA', 'ZAGREB', 'NICOSIA', 'PRAQUE', 'COPENHAGEN', 'TALLINN', 'HELSINKI', 'PARIS', 'BERLIN', 'ATHENS', 'BUDAPEST', 'DUBLIN', 'ROME', 'RIGA', 'VILNIUS', 'LUXEMBOURG', 'SKOPJE', 'VALETTA', 'AMSTERDAM', 'WARSAW', 'LISBON', 'BUCHAREST', 'BRATISLAVA', 'LJUBLJANA', 'MADRID', 'STOCKHOLM', 'LONDON')
+    return random.choice(capitals).upper()
+
+
+def replace_dashes_with_letters(capital, guessed, letter):
+
+    """for chosen capital replace dashes with correctly geussed letters"""
+    out = ""
+    for letter in capital:
+        if letter in guessed:
+            out = out + letter
+        else:
+            out = out + " '_' "
+    return out
+
+
+def clear_n_print_arts(lifes):
+
+    """clear screen and print hangman arts"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(arts[9-lifes])
+
+
+def play_again():
+
+    """asks if user wants to play the game again"""
+    again = input("\nPress Y to play again or any key to quit. ").upper()
+    if (again == "Y"):
+        pass
+    else:
+        sys.exit()
+    return
+
+
+def hang_main():
+
+    while True:
+
+        capital = get_capital()
+        guessed = []    # list of right letters already guessed
+        wrong = []      # list of wrong letters already guessed
+
+        # start lifes and timer
+        lifes = 10
+        print('"_"' * len(capital), "\n You have", lifes, "lifes left")
+        start_time = time.time()
+
+        while lifes > 0:
+
+            guess = input("\nGuess a letter at name of capital or full name:").upper()
+
+            if guess == capital:
+                out = guess
+                print(out)
+                break
+            dashed_capital = replace_dashes_with_letters(capital, guessed, guess)
+            if guess in guessed:
+                print("\nAlready guessed", guess)
+            elif guess in wrong:
+                print("\nAlready guessed", wrong)
+                lifes = lifes - 1
+                clear_n_print_arts(lifes)
+                print("\nWrong letters you\'ve already used:", wrong)
+                print("\nYou\'ve already used this wrong letters:", wrong)
+            elif guess in capital:
+                guessed.append(guess)
+                dashed_capital = replace_dashes_with_letters(capital, guessed, guess)
+                print(dashed_capital)
+                if dashed_capital == capital:
+                    print(lifes, "chances left")
+                    break
+
+            else:
+                print("\nNo")
+                lifes = lifes - 1
+                clear_n_print_arts(lifes)
+                wrong.append(guess)
+                print("\nWrong letters you\'ve already used:", wrong)
+
+        if lifes:
+            end_time = time.time() - start_time
+            print("\nYou guessed ", capital, " it took you {} seconds to guess the number and you have {} lifes left "
+                  .format(round(end_time), lifes))
+            # zgadłeś, coś dostajesz,może klucz czy coś?
+            sys.exit()
+        else:
+            print("\nYou didn't get ", capital, " sorry you are HANGMAN!")
+            again = play_again()
+
+
+hang_main()
